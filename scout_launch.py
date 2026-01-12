8d1d9b667c4eabe53e441da08f1b12af5b2e3d8d
Jiseon Kim <ji.dozin@gmail.com>
2026-01-12 18:37:30 +0900

    Create scout_launch.py

---
+++ scout_launch.py
@@ -0,0 +1,4 @@
+source /when2reason/install/scout_mini_base/share/scout_mini_base/local_setup.bash
+source /when2reason/install/scout_mini_description/share/scout_mini_description/local_setup.bash
+sudo ip link set can0 up type can bitrate 500000
+ros2 launch scout_mini_base base_launch.py
